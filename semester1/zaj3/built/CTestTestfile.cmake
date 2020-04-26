de30d4bb6abd919b7d198e0f24e1f5c9c5ab8896
karmazynow-a <aleks.poreba97@gmail.com>
2020-04-26 12:10:04 +0200

    Clean up the mess that this repo was

--- semester1/zaj3/built/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/stud2016/6poreba/ppo/zaj3/src
-# Build directory: /home/stud2016/6poreba/ppo/zaj3/built
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(next "test_next")
